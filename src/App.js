f163ab7e2eedc5a9df2aa91d6c32373fe6017068
Daniel-Slavnyi <dslavnyj00@gmail.com>
2022-12-13 15:29:38 +0200

    task1-1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+
 
 function App() {
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
-          {isOpen && (
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-          )}
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-
-          <ul>
-            {data.map((item) => (
-              <li key={item.id}>{item.name}</li>
-            ))}
-          </ul>
-        </header>
-      </div>
+      <h2 className="header-title" style={{fontSize: "24px", color: "tomato", textAlign: "center"}}>"Котик на дієті - сумний котик"</h2>
     </>
   );
 }
