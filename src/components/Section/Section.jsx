046c4da5261d2aa57626b6b1abaadcba4e244fe3
gtgambit <gtgambit@gmail.com>
2022-12-14 16:30:45 +0200

    updates
    updates

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,18 +1,20 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import Container from "../Container/Container";
 
-
-const Section = ({title, children}) =>{
-
- return (
- <section>
-    <h2>{title}</h2> 
-     {children}
-</section>)
-}
+const Section = ({ title, children }) => {
+  return (
+    <section>
+      <Container>
+        <h2>{title}</h2>
+        {children}
+      </Container>
+    </section>
+  );
+};
 
 Section.propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired,
-}
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 export default Section;
