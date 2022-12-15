9ce8affeb57f26ac3b56aceefb997d46782b51c9
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-12-15 20:02:23 +0200

    Finish task

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,40 +1,88 @@
 import { Component } from "react";
-import AccordionItem from '../AccordionItem/AccordionItem';
-import style from './FAQ.module.css'
+import AccordionItem from "../AccordionItem/AccordionItem";
+import style from "./FAQ.module.css";
 export class FAQ extends Component {
   state = {
-    isAllOpen: false
+    isAllOpen: true,
+    buttonPushed: false,
   };
-  onOpenAll = () =>{
+  onOpenAll = () => {
     this.setState({
-      isAllOpen: true
-    })
-  }
-  onCloseAll = () =>{
+      isAllOpen: true,
+      buttonPushed: true,
+    });
+  };
+  onCloseAll = () => {
     this.setState({
-      isAllOpen: false
-    })
-  }
+      isAllOpen: false,
+      buttonPushed: true,
+    });
+  };
+  onTriggerButton = () => {
+    this.setState({
+      buttonPushed: false,
+    });
+  };
   render() {
-  return (
-    <div className={style.accordion}>
-      <h1>FAQ</h1>
-      <button type="button" className={style.btn} id="expand-all" onClick={this.onOpenAll}>
-        Expand All
-      </button>
-      <button type="button" className={style.btn} id="collapse-all"onClick={this.onCloseAll}>
-        Collapse All
-      </button>
-      <ul className={style.FAQList}>
-     <li className={style.FAQItem}><AccordionItem isAllOpen={this.state.isAllOpen} /></li>
-
-        <li className={style.FAQItem}><AccordionItem isAllOpen={this.state.isAllOpen} /></li>
-        <li className={style.FAQItem}><AccordionItem isAllOpen={this.state.isAllOpen} /></li>
-        <li className={style.FAQItem}><AccordionItem isAllOpen={this.state.isAllOpen} /></li>
-        <li className={style.FAQItem}><AccordionItem isAllOpen={this.state.isAllOpen} /></li>
-      </ul>
-    </div>
-  );
-}}
+    return (
+      <div className={style.accordion}>
+        <h1>FAQ</h1>
+        <button
+          onClick={this.onOpenAll}
+          type="button"
+          className={style.btn}
+          id="expand-all"
+        >
+          Expand All
+        </button>
+        <button
+          onClick={this.onCloseAll}
+          type="button"
+          className={style.btn}
+          id="collapse-all"
+        >
+          Collapse All
+        </button>
+        <ul className={style.FAQList}>
+          <li className={style.FAQItem}>
+            <AccordionItem
+              buttonPushed={this.state.buttonPushed}
+              onTriggerButton={this.onTriggerButton}
+              isAllOpen={this.state.isAllOpen}
+            />
+          </li>
+          <li className={style.FAQItem}>
+            <AccordionItem
+              buttonPushed={this.state.buttonPushed}
+              onTriggerButton={this.onTriggerButton}
+              isAllOpen={this.state.isAllOpen}
+            />
+          </li>
+          <li className={style.FAQItem}>
+            <AccordionItem
+              buttonPushed={this.state.buttonPushed}
+              onTriggerButton={this.onTriggerButton}
+              isAllOpen={this.state.isAllOpen}
+            />
+          </li>
+          <li className={style.FAQItem}>
+            <AccordionItem
+              buttonPushed={this.state.buttonPushed}
+              onTriggerButton={this.onTriggerButton}
+              isAllOpen={this.state.isAllOpen}
+            />
+          </li>
+          <li className={style.FAQItem}>
+            <AccordionItem
+              buttonPushed={this.state.buttonPushed}
+              onTriggerButton={this.onTriggerButton}
+              isAllOpen={this.state.isAllOpen}
+            />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default FAQ;
