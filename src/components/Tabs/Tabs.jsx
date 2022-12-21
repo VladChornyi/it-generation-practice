15715d3da042603670b649514590ea481d9312a0
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2022-12-21 15:05:03 +0200

    Update 7.3 Tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,38 +1,62 @@
-import { Component } from "react"
-import "./Tabs.css"
-import clsx from "clsx"
-import { TabsItemBottom } from "../TabsItemBottom/TabsItemBottom"
-import { TabsItemTop } from "../TabsItemTop/TabsItemTop"
+import { Component } from "react";
+import "./Tabs.css";
+import clsx from "clsx";
+import { TabsItemBottom } from "../TabsItemBottom/TabsItemBottom";
+import { TabsItemTop } from "../TabsItemTop/TabsItemTop";
 
-const arrOfIds = ["1", "2", "3", "4"]
+const arrOfIds = ["1", "2", "3", "4"];
 
 export default class Tabs extends Component {
-	state = {
-		activeId: "1",
-	}
+  state = {
+    activeId: localStorage.getItem("activeId") ?? "1",
+  };
 
-	handleClick = (e) => {
-		console.log(e.target.dataset.id)
-		this.setState({
-			activeId: e.target.dataset.id,
-		})
-	}
+  //   componentDidMount() {
+  //     const localActiveId = localStorage.getItem("activeId");
+  //     if (localActiveId) {
+  //       this.setState({
+  //         activeId: localActiveId,
+  //       });
+  //     }
+  //   }
 
-	render() {
-		const styles = clsx(this.state.isActive && "active")
-		return (
-			<>
-				<div className="tabs">
-					<ul className="tabs__caption">
-						{arrOfIds.map((id) => (
-							<TabsItemTop key={id} id={id} onClick={this.handleClick} activeId={this.state.activeId} />
-						))}
-					</ul>
-					{arrOfIds.map((id) => (
-						<TabsItemBottom key={id} id={id} onClick={this.handleClick} activeId={this.state.activeId} />
-					))}
-				</div>
-			</>
-		)
-	}
+  componentDidUpdate(_, prevstate) {
+    if (this.state.activeId !== prevstate.activeId) {
+      localStorage.setItem("activeId", this.state.activeId);
+    }
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      activeId: e.target.dataset.id,
+    });
+  };
+
+  render() {
+    const styles = clsx(this.state.isActive && "active");
+    return (
+      <>
+        <div className="tabs">
+          <ul className="tabs__caption">
+            {arrOfIds.map((id) => (
+              <TabsItemTop
+                key={id}
+                id={id}
+                onClick={this.handleClick}
+                activeId={this.state.activeId}
+              />
+            ))}
+          </ul>
+          {arrOfIds.map((id) => (
+            <TabsItemBottom
+              key={id}
+              id={id}
+              onClick={this.handleClick}
+              activeId={this.state.activeId}
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
 }
