af677754f5045fd58c106cfd7e0d21cbad077fb5
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2023-01-10 15:34:15 +0200

    Create input with search

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+// import React, { useEffect } from "react";
+// import { useState } from "react";
+
+import { Link, useSearchParams } from "react-router-dom";
+
+const tasks = [
+  {
+    name: "posts",
+    link: "posts",
+  },
+  {
+    name: "products",
+    link: "products",
+  },
+  {
+    name: "albums",
+    link: "albums",
+  },
+  {
+    name: "starWars",
+    link: "starWars",
+  },
+  {
+    name: "goodsList",
+    link: "goodsList",
+  },
+  {
+    name: "FAQ",
+    link: "FAQ",
+  },
+];
 
 export default function HomePage() {
+  const [params, setParams] = useSearchParams();
+
+  const taskName = params.get("search") ?? "";
+
+  const filteredTasks = tasks.filter((task) =>
+    task.name.toLowerCase().includes(taskName)
+  );
+
+  const taskNameChange = (event) => {
+    setParams({ search: event.target.value.toLowerCase() });
+  };
+
   return (
-    <div>HomePage</div>
-    
-  )
+    <>
+      <div>HomePage</div>
+      <input
+        name="taskName"
+        placeholder="Type task name"
+        type="text"
+        onChange={taskNameChange}
+        value={taskName}
+      />
+      {filteredTasks.length > 0 && (
+        <ul>
+          {filteredTasks.map((task) => (
+            <li>
+              <Link to={`exercises/${task.link}`}>{task.name}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
