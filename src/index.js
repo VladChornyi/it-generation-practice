8f2dde97080f0fba4d4d92c826a61f22222eb6da
Round Sound <17480523+Wonderband@users.noreply.github.com>
2023-01-13 15:34:20 +0200

    Revert "15lesson"

--- src/index.js
+++ src/index.js
@@ -4,21 +4,17 @@ import "./index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { persistor, store } from "./redux/store";
-import { PersistGate } from 'redux-persist/integration/react';
-
+import { store } from "./redux/store";
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
     <Provider store={store}>
     <App />
     </Provider>
-      </BrowserRouter>
-    </PersistGate>
+    </BrowserRouter>
   </React.StrictMode>
 );
